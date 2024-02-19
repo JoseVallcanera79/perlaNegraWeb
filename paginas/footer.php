083f6359121f9eaecc1f5e03a0a49748e30e8add
jose ramon <jazz_crack@hotmail.com>
2024-02-19 16:22:55 +0100

    todos los archivos de comida y bebida, footer, head

---
+++ paginas/footer.php
@@ -0,0 +1,32 @@
+<footer class=" col-12 col-6 col-3">
+
+
+
+    <figure class="snip1566">
+        <img src="/images/mapaPerla.png" alt="sq-sample19" />
+        <figcaption><i class="ion-android-star-outline"></i></figcaption>
+        <a href="https://www.google.com/maps/dir//Platja,+Carrer+del+Cap+Blanc,+46400+Cullera,+Valencia/@39.1817902,-0.3097176,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0xd61c8cc5b14aa9f:0x7d56df5f11957286!2m2!1d-0.2273167!2d39.1818193?entry=ttu"
+            target="_blank"></a>
+    </figure>
+
+
+
+
+
+    <div class="icons">
+
+
+        <a href="https://www.facebook.com/perlanegrabeach" target="_blank">
+            <i class="fab fa-facebook"></i>
+        </a>
+
+        <a href="https://www.instagram.com/perlanegracullera/" target="_blank">
+            <i class="fab fa-instagram"></i>
+        </a>
+
+
+    </div>
+    <div class="copyright">
+        <p>© 2024 La Perla Negra &nbsp &nbsp | &nbsp &nbsp Precios con IVA incluido.</p>
+    </div>
+</footer>
---
+++ paginas/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maxium-scale=1">
+    <title>Perla Negra</title>
+    <!-- all.css loads all styles and icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- Enlace al archivo CSS -->
+    <link rel="stylesheet" href="styles.css">
+    <!-- Enlace al archivo CSS de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- favicon web-->
+    <link rel="icon" type="image/x-icon" href="/images/icons/cocktail.png">
+    <!-- jQuery (es requerido por Bootstrap) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Opcional: Enlace al archivo JavaScript de Bootstrap -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
